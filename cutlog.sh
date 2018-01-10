28b90a0f14ca98481fa6d94d789208f052856851
root <itadminlx@163.com>
2018-01-10 16:31:54 +0800

    check

---
+++ cutlog.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+date=$(date +%Y%m%d -d -1day)
+cd /www/wdlinux/nginx/logs/
+if [ ! -d cut ] ; then
+        mkdir cut
+fi
+mv yumao.com_access.log cut/www.alinx.com-access_$(date +%Y%m%d -d -1day).log
+mv yumao.com_error.log cut/www.alinx.com-error_$(date +%Y%m%d -d -1day).log
+service nginxd stop && service nginxd start && service nginxd status
+if [ $? -eq 0 ];then
+         echo "`date`_logcut done/nginxd.log
+fi
