6f6cba33be0ae30d1e5122bac8cfb2f47763fac2
Alinx <30387309+GZ-Alinx@users.noreply.github.com>
2018-03-01 14:23:12 +0800

    Update cutlog.sh

--- cutlog.sh
+++ cutlog.sh
@@ -4,8 +4,8 @@ cd /www/wdlinux/nginx/logs/
 if [ ! -d cut ] ; then
         mkdir cut
 fi
-mv yumao.com_access.log cut/www.alinx.com-access_$(date +%Y%m%d -d -1day).log
-mv yumao.com_error.log cut/www.alinx.com-error_$(date +%Y%m%d -d -1day).log
+mv alinx.com_access.log cut/www.alinx.com-access_$(date +%Y%m%d -d -1day).log
+mv alinx.com_error.log cut/www.alinx.com-error_$(date +%Y%m%d -d -1day).log
 service nginxd stop && service nginxd start && service nginxd status
 if [ $? -eq 0 ];then
          echo "`date`_logcut done/nginxd.log
